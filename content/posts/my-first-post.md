6e703cd605920dea59bc81e6f27ccd1de9ff17a2
Eko Sutrisno <eko@briix.com>
2022-09-14 13:46:52 +0700

    add: added all assets

---
+++ content/posts/my-first-post.md
@@ -0,0 +1,6 @@
+---
+title: "My First Post"
+date: 2022-08-10T09:32:58+07:00
+draft: true
+---
+
---
+++ themes/thisThemeIsNew/tailwind.config.js
@@ -0,0 +1,30 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["content/**/*.md", "layouts/**/*.html"],
+  theme: {
+    extend: {
+      colors: {
+        'briix-primary': '#1F9ED3',
+        'briix-black': '#000000',
+        'briix-white': '#FFFFFF',
+        'briix-body': '#666666',
+
+        'briix-blue-1': '#EAF6FB',
+        'briix-blue-6': '#135F7F',
+        'briix-blue-7': '#0C3F54',
+
+        'briix-grey-2': '#E3E3E3',
+        'briix-grey-6': '#606060',
+        'briix-grey-7': '#262626',
+
+        'briix-black-100': '#333333',
+      },
+
+      fontFamily: {
+        inter: ['Inter', 'sans-serif'],
+        manrope: ['Manrope', 'sans-serif']
+      }
+    },
+  },
+  plugins: [],
+}
